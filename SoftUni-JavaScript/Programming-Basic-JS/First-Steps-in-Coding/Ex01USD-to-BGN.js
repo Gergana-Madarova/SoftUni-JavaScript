ff5edfb2b382ea2037d4e54a7ea9cd63426879eb
Gergana-Madarova <g.madarova@gmail.com>
2021-05-15 00:50:02 +0300

    Created JS

--- SoftUni-JavaScript/Programming-Basic-JS/First-Steps-in-Coding/Ex01USD-to-BGN.js
+++
@@ -1,9 +0,0 @@
-function usdToBgn(input) {
-    let usd = Number(input);
-
-    let bgn = usd * 1.79549;
-
-    console.log(bgn.toFixed(2));
-}
-
-usdToBgn("22");
--- SoftUni-JavaScript/Programming-Basic-JS/First-Steps-in-Coding/L02Nums-1-10-with-for.js
+++
@@ -1,7 +0,0 @@
-function numbers1To10() {
-    for (let i = 1; i < 11; i++) {
-        console.log(i);
-    }
-}
-
-numbers1To10();

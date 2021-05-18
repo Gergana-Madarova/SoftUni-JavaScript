56df6f5cd819f66b77255d410e931902e40115bd
Gergana-Madarova <g.madarova@gmail.com>
2021-05-18 13:07:12 +0300

    Added L04Number-100-200

---
+++ SoftUni-JavaScript/Programming-Basic-JS/Conditional-Statements/L04Number-100-200.js
@@ -0,0 +1,13 @@
+function numers(num) {
+    //let num = Number(number);
+
+    if (num > 200) {
+        console.log("Greater than 200");
+    } else if (num < 100) {
+        console.log("Less than 100");
+    } else {
+        console.log("Between 100 and 200");
+    }
+}
+
+numers(21);

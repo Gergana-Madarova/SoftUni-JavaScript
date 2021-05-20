b455d7275b71a6fd6eb4d0f7469131b12362c6e6
Gergana-Madarova <g.madarova@gmail.com>
2021-05-20 11:43:17 +0300

    Added L02Weekend-or-Working-Day

---
+++ SoftUni-JavaScript/Programming-Basic-JS/Conditional-Statements-Advanced/L02Weekend-or-Working-Day.js
@@ -0,0 +1,22 @@
+function weekendOrWorkindDay(day) {
+    switch (day) {
+        case "Monday":
+        case "Tuesday":
+        case "Wednesday":
+        case "Thursday":
+        case "Friday":
+            console.log("Working day");
+            break;
+        case "Saturday":
+        case "Sunday":
+            console.log("Weekend");
+            break;
+        default:
+            console.log("Error");
+            break;
+    }
+}
+
+weekendOrWorkindDay("Wednesday");
+weekendOrWorkindDay(("April"));
+weekendOrWorkindDay("Monday");

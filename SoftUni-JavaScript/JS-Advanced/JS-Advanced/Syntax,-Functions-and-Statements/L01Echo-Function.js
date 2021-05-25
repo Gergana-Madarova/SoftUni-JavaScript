11c59ac697fb181783238926e51f777564c0e1cf
Gergana-Madarova <g.madarova@gmail.com>
2021-05-25 12:16:57 +0300

    Added L01Echo-Function

---
+++ SoftUni-JavaScript/JS-Advanced/JS-Advanced/Syntax,-Functions-and-Statements/L01Echo-Function.js
@@ -0,0 +1,9 @@
+function echoFunction(input) {
+    let text = input;
+
+    console.log(text.length);
+    console.log(text);
+}
+
+echoFunction('Hello, JavaScript!');
+echoFunction('strings are easy');

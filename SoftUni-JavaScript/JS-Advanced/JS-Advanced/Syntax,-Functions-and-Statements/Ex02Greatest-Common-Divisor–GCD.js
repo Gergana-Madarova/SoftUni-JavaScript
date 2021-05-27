8454d34558721ae1cee0318428a5a4dd981a58d5
Gergana-Madarova <g.madarova@gmail.com>
2021-05-27 12:04:37 +0300

    Added Ex02Greatest-Common-Divisor–GCD

---
+++ SoftUni-JavaScript/JS-Advanced/JS-Advanced/Syntax,-Functions-and-Statements/Ex02Greatest-Common-Divisor–GCD.js
@@ -0,0 +1,12 @@
+function greatestCommonDivisor(num1, num2) {
+    while (num2 > 0) {
+        let temp = num2;
+        num2 = num1 % num2;
+        num1 = temp;
+    }
+
+    return num1;
+}
+
+console.log(greatestCommonDivisor(15, 5));
+console.log(greatestCommonDivisor(2154, 458));

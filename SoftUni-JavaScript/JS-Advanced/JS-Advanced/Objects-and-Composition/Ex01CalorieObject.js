6a191e394c1164feed8b30cf4103b25eaecbaa6d
Gergana-Madarova <g.madarova@gmail.com>
2021-06-06 22:48:02 +0300

    Added Ex01CalorieObject

---
+++ SoftUni-JavaScript/JS-Advanced/JS-Advanced/Objects-and-Composition/Ex01CalorieObject.js
@@ -0,0 +1,14 @@
+function calorieObject(input) {
+    const foodCalorie = {};
+
+    for (let index = 0; index < input.length - 1; index+=2) {
+        let [name] = input[index].split(', ');
+        //let [calorie] = input[index + 1];
+        foodCalorie[name] = Number(input[index + 1]);
+    }
+
+    console.log(foodCalorie);
+}
+
+calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);
+calorieObject(['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42']);

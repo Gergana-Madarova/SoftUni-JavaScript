060b26116886d0596b37960ac9fe5bff7be4e1f1
Gergana-Madarova <g.madarova@gmail.com>
2021-06-14 23:33:45 +0300

    Added L01AreaAndVolumeCalculator

---
+++ SoftUni-JavaScript/JS-Advanced/JS-Advanced/Advanced-Functions/L01AreaAndVolumeCalculator.js
@@ -0,0 +1,33 @@
+function area() {
+    return Math.abs(this.x * this.y);
+};
+
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+};
+
+function solve(area, vol, input) {
+    const objects = JSON.parse(input);
+    let result = [];
+
+    for (const obj of objects) {
+        result.push({
+            area: area.call(obj),
+            volume: vol.call(obj)
+        });
+    }
+
+    return result;
+}
+
+console.log(solve(area, vol, `[
+    {"x":"1","y":"2","z":"10"},
+    {"x":"7","y":"7","z":"10"},
+    {"x":"5","y":"2","z":"10"}]`));
+
+solve(area, vol, `[
+    {"x":"10","y":"-22","z":"10"},
+    {"x":"47","y":"7","z":"-5"},
+    {"x":"55","y":"8","z":"0"},
+    {"x":"100","y":"100","z":"100"},
+    {"x":"55","y":"80","z":"250"}]`);
